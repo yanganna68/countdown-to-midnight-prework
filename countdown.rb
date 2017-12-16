4e88f9a77ba92863ed698853e27f57193b4d4e37
ANNA YANG <yanganna68@gmail.com>
2017-12-16 04:02:54 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(num)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep (num)
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
+end
